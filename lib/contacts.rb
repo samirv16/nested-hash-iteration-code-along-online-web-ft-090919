fe8f33eddee0fa88021a5e55d4f0d596515c98b5
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-14 13:50:15 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -20,7 +20,7 @@ def remove_strawberry(contacts)
   data.each do |attribute, value|
     if attribute == :favorite_ice_cream_flavors
     value.each do |flavor|
-      
+  end
     end
     end
   end
